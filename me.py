cf46ae4bd00d74535a0d1a3167677d1e02fce2b2
Gourav Das <gouravdasrr@gmail.com>
2024-08-15 11:17:40 +0530

    Add files via upload

---
+++ me.py
@@ -0,0 +1,7 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
+
+
+130d1efcc2024a5e9b30d4cdb8f206d1
---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "new divide":"https://www.youtube.com/watch?v=ysSxxIqKNN0&pp=ygUXbmV3IGRpdmlkZSBsaW5raW4gcGFyayA%3D",
+    "numb": "https://www.youtube.com/watch?v=kXYiU_JCYtU&pp=ygUNIGxpbmtpbiBwYXJrIA%3D%3D",
+    "in the end": "https://www.youtube.com/watch?v=eVTXPUF4Oz4&pp=ygUNIGxpbmtpbiBwYXJrIA%3D%3D"
+}
